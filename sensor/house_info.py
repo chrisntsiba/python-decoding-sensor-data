e8411aceecb51907b29df276ce2602ae3912229c
ChristianNtsiba <cntsiba@outlook.com>
2021-10-12 23:05:04 -0400

    module 2 updates

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -14,7 +14,7 @@ class HouseInfo:
         # e.g. when the field input parameter is set to "id" then, the record[field] value corresponds to the "id" column values. 
         # In this method, the rec_area variable maps to 'area' column values.
         for record in self.data:
-            if(rec_area==0):
+            if rec_area == 0:
                 field_data.append(record[field])
             elif(rec_area==int(record["area"])):
                 field_data.append(record[field])
@@ -24,8 +24,9 @@ class HouseInfo:
     # In this method, the rec_date input parameter maps to the 'date' column's values.
     def get_data_by_date(self, field, rec_date=datetime.today()):
         field_data = []
+
         for record in self.data:
-            if(record["date"] == rec_date.strftime("%m/%d/%y")):
+            if record["date"] == rec_date.strftime("%m/%d/%y"):
                 field_data.append(record)
 
         return field_data
